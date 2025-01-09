623aa8c4664db1823606c83e9066a8d043b2a5a1
mohdshoaibkhan72 <mohdshoaibkhan7211@gmail.com>
2025-01-10 02:33:09 +0530

    Add Services and Why Choose Us sections with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Landing from "./Components/Landing/Landing";
 import AboutUs from "./Components/AboutUs/AboutUs";
+import Services from "./Components/services/Services";
+import WhyChooseUs from "./Components/whychoseus/whychoseus";
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Landing></Landing>
       <AboutUs></AboutUs>
+      <Services></Services>
+      <WhyChooseUs></WhyChooseUs>
     </>
   );
 }
