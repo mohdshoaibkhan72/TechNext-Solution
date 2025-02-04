14e5e28902cf7ccee9994e007481958dadfb76a4
mohdshoaibkhan72 <mohdshoaibkhan7211@gmail.com>
2025-02-04 12:51:57 +0530

    Fix favicon paths in index.html and dist/index.html, and update logo import in Navbar component for consistency

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../images/1 copy.png";
+import logo from "../../images/1copy.png";
 import "./Navbar.css";
 
 function Navbar() {
